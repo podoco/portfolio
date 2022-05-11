d6afb1bd794ccb4d8340ea0fa74197763f1bc816
mingijeon <jeonmingi83@naver.com>
2022-05-11 14:09:53 +0900

    JS:Home_contact_btn

--- main.js
+++ main.js
@@ -20,8 +20,16 @@ navbarMenu.addEventListener('click', (event) =>{
     if (link == null){
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
-
+    scrollIntoView(link);
 })
+
+//Handle click on 'contact me button on home
+const contectbtn = document.querySelector('.home_contact');
+contectbtn.addEventListener('click', (event)=>{
+    scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
