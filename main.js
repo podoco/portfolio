138e90ac8640c9112e9729367ba1891c749f6f69
mingijeon <jeonmingi83@naver.com>
2022-05-11 15:30:26 +0900

    JS:Arrow_up

--- main.js
+++ main.js
@@ -38,7 +38,23 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () =>{
 
     home.style.opacity = 1 - window.scrollY / homeHeight;
-});
+})
+
+//Show 'arrow up' button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll',()=>{
+    if (window.scrollY >homeHeight /2){
+        arrowUp.classList.add('visible');
+    } else{
+        arrowUp.classList.remove('visible')
+    }
+})
+//Handle click on the "arrow up"button
+arrowUp.addEventListener('click',(event)=>{
+    scrollIntoView('#home');
+})
+
+
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
