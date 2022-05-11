b007135726cc9917cbcea99a2cd46def5a0d92c3
mingijeon <jeonmingi83@naver.com>
2022-05-11 16:46:10 +0900

    JS:Filter

--- main.js
+++ main.js
@@ -54,6 +54,25 @@ arrowUp.addEventListener('click',(event)=>{
     scrollIntoView('#home');
 })
 
+//projects
+const workBtnContainer = document.querySelector('.work_categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click',(e)=>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    projects.forEach((project) =>{
+        console.log(project.dataset.type);
+        if(filter =='*' || filter == project.dataset.type){
+            project.classList.remove('invisible');
+        }else{
+            project.classList.add('invisible');
+        }
+    });
+});
 
 
 function scrollIntoView(selector){
