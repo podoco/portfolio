4c5c762395d0fbd81f33f55eb8a0d775935219e1
mingijeon <jeonmingi83@naver.com>
2022-05-12 23:12:25 +0900

    JS:Project filtering

--- main.js
+++ main.js
@@ -64,14 +64,18 @@ workBtnContainer.addEventListener('click',(e)=>{
     if(filter == null){
         return;
     }
-    projects.forEach((project) =>{
-        console.log(project.dataset.type);
-        if(filter =='*' || filter == project.dataset.type){
-            project.classList.remove('invisible');
-        }else{
-            project.classList.add('invisible');
-        }
-    });
+    projectContainer.classList.add('anim-out');  
+    setTimeout(() =>{
+        projects.forEach((project) =>{
+            console.log(project.dataset.type);
+            if(filter =='*' || filter == project.dataset.type){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    },300);
 });
 
 
