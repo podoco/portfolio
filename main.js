01e4c37a41aa210b8151d51da6aa297b1eadf3c9
mingijeon <jeonmingi83@naver.com>
2022-05-13 02:15:14 +0900

    JS:navbar_menu_toggle+UI tweak

--- main.js
+++ main.js
@@ -13,14 +13,21 @@ document.addEventListener('scroll', () => {
     }
 })
 
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click',() =>{
+    navbarMenu.classList.toggle('open');
+}) 
+
 // Handle scrolling when taping on the navbar menu
-const navbarMenu = document.querySelector('.navbar_menu');
+const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) =>{
     const target = event.target;
     const link = target.dataset.link;
     if (link == null){
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 })
 
