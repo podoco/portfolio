3718cbd8674ff2aa6d9e0e4c0815a5e145032f42
mingijeon <jeonmingi83@naver.com>
2022-05-14 11:33:53 +0900

    Final_Touch

--- main.js
+++ main.js
@@ -99,11 +99,75 @@ workBtnContainer.addEventListener('click',(e)=>{
 });
 
 
-function scrollIntoView(selector){
+
+
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonials',
+    '#contact',
+  ];
+  const sections = sectionIds.map(id => document.querySelector(id));
+  const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+  
+  let selectedNavIndex = 0;
+  let selectedNavItem = navItems[0];
+
+  function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+  }
+  
+  function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
 
-//1.모든 섹션 요소들을 가지고온다
-//2.intersectionObserver를 이용해서 섹션관찰
-//3.보여지는 섹션에 해당하는 메뉴 아이템 활성화
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+  };
+  
+  const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        // 스크롤링이 아래로 되어서 페이지가 올라옴
+        if (entry.boundingClientRect.y < 0) {
+          selectedNavIndex = index + 1;
+        } else {
+          selectedNavIndex = index - 1;
+        }
+      }
+    });
+  };
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+
+    if (window.scrollY === 0) {
+
+    selectedNavIndex = 0;
+
+    } else if (
+
+        Math.round(window.scrollY + window.innerHeight) >=
+
+        document.body.clientHeight
+
+    ) {
+    selectedNavIndex = navItems.length-1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
